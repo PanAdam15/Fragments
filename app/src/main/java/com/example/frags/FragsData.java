0af759dfd2261b2519c3fa0980a2c2ec640d616f
PanAdam15 <adamxpankowski@gmail.com>
2022-04-20 17:05:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/frags/FragsData.java
@@ -0,0 +1,7 @@
+package com.example.frags;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+public class FragsData extends ViewModel {
+    public final MutableLiveData<Integer> counter = new MutableLiveData<>(0);
+}
